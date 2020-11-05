e8f1e252866dac96a760d153cbad6c03e42aac51
Sokos6 <will.sokolowski@gmail.com>
2020-11-04 18:31:44 -0600

    react is working but where is my index nerds

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div className='app'>
+      <h2>HERE WE ARE</h2>
+    </div>
+  );
+}
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
       },
     ],
   },
+  mode: 'development',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
   },
