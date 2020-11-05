1109d4410d90f4df9bac9aed7f084673715ccebc
Sokos6 <will.sokolowski@gmail.com>
2020-11-04 22:29:36 -0600

    project update before redux

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios.get('http://localhost:3000/users').then((response) => {
+      console.log(response.data);
+    });
+  });
+
   return (
     <div className='app'>
       <h2>HERE WE ARE</h2>
